912d4be1718678ffb5461e5f1fef6cd3b6e96035
iwindd <freewinendlife@gmail.com>
2024-08-31 18:30:07 +0700

    feat<users>: datatable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 class UserController extends Controller
 {
     // Users
     public function index()
     {
+        if (request()->ajax()) {
+            return datatables()->of(
+                User::all()
+            )
+                ->addColumn("action", "components.users.action")
+                ->rawColumns(['action'])
+                ->addIndexColumn()
+                ->make(true);
+        }
 
         return view('admin.users.index');
     }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
-            <h2 class="text-xl font-semibold leading-tight">
+            <h2 class="text-xl font-semibold leading-tight ">
                 {{ __('users.heading') }}
             </h2>
 
@@ -16,30 +16,5 @@
         </div>
     </x-slot>
 
-    <table id="myTable" class="display">
-        <thead>
-            <tr>
-                <th>Column 1</th>
-                <th>Column 2</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Row 1 Data 1</td>
-                <td>Row 1 Data 2</td>
-            </tr>
-            <tr>
-                <td>Row 2 Data 1</td>
-                <td>Row 2 Data 2</td>
-            </tr>
-        </tbody>
-    </table>
-
-    <x-slot name="scripts">
-        <script>
-            $(document).ready( function () {
-                $('#myTable').DataTable();
-            } );
-        </script>
-    </x-slot>
+    <x-users.datatable />
 </x-app-layout>
