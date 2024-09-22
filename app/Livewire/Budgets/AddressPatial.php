4b9fbc4ce49b510252cff1dcde08a9ffc7118e2c
iwindd <freewinendlife@gmail.com>
2024-09-23 06:12:58 +0700

    refactor<address>: idk what that shit i will rework them later

--- app/Livewire/Budgets/AddressPatial.php
+++ app/Livewire/Budgets/AddressPatial.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Budgets;
 
+use App\Models\Location;
 use Livewire\Component;
 
 class AddressPatial extends Component
@@ -9,21 +10,22 @@ class AddressPatial extends Component
     public $data;
     public $errors;
     public $old;
+    public $locations;
     private $template = [
-        [
-            'from_location_id' => '',
-            'from_date' => '',
-            'back_location_id' => '',
-            'back_date' => ''
-        ]
+        'from_location_id' => '',
+        'from_date' => '',
+        'back_location_id' => '',
+        'back_date' => ''
     ];
 
 
     public function mount($errors, $old)
     {
-        $this->data[] = [$this->template];
+        $this->data[] = $this->template;
         $this->errors = $errors;
         $this->old = $old;
+        $this->locations = [];
+        $usingLocations = collect([]);
 
         if ($old) {
             foreach ($old as $index => $payload) {
@@ -33,14 +35,22 @@ class AddressPatial extends Component
                     }
 
                     $this->data[$index][$key] = $value;
+
+                    if ($key == "from_location_id" || $key == "back_location_id") {
+                        $usingLocations->push($value);
+                    }
                 }
             }
+        }
+
+        if ($usingLocations->count() > 1) {
+            $this->locations = Location::whereIn("id", $usingLocations)->get(['id', 'label'])->pluck('label', 'id')->toArray();
         }
     }
 
     public function addAddress()
     {
-        $this->data[] = [$this->template];
+        $this->data[] = $this->template;
     }
 
     public function removeAddress($index)
--- resources/views/livewire/budgets/address-patial.blade.php
+++ resources/views/livewire/budgets/address-patial.blade.php
@@ -4,28 +4,42 @@
             <section wire:key="address-{{ $index }}" class="grid grid-cols-4 gap-2 mt-2">
                 <section class="space-y-2"> {{-- FROM --}}
                     <x-form.label for="data.{{ $index }}.from_location_id" :value="__('budgets.input-address-from')" />
-                    <select class="location-selector w-full outline-none" wire.model="data.{{ $index }}.from_location_id"
-                        name="address[{{ $index }}][from_location_id]"></select>
-                    <x-form.error :messages="$errors['address.'.$index.'.from_location_id'][0] ?? ''" />
+                    <select data-key="from_location_id" class="location-selector w-full outline-none"
+                        wire:model.live="data.{{ $index }}.from_location_id"
+                        name="address[{{ $index }}][from_location_id]">
+                        @isset($old[$index]['from_location_id'])
+                            <option value="{{ $old[$index]['from_location_id'] }}" selected>
+                                {{ $locations[$old[$index]['from_location_id']] }}</option>
+                        @endisset
+                    </select>
+                    <x-form.error :messages="$errors['address.' . $index . '.from_location_id'][0] ?? ''" />
                 </section>
                 <section class="space-y-2"> {{-- FROM_DATE --}}
                     <x-form.label for="data.{{ $index }}.from_date" :value="__('budgets.input-address-from-datetime')" />
                     <x-form.input id="data.{{ $index }}.from_date" :value="$old[$index]['from_date'] ?? ''"
+                        wire:model.defer="data.{{ $index }}.from_date"
                         name="address[{{ $index }}][from_date]" type="datetime-local" class="block w-full" />
-                    <x-form.error :messages="$errors['address.'.$index.'.from_date'][0] ?? ''" />
+                    <x-form.error :messages="$errors['address.' . $index . '.from_date'][0] ?? ''" />
                 </section>
                 <section class="space-y-2"> {{-- BACK --}}
                     <x-form.label for="data.{{ $index }}.back_location_id" :value="__('budgets.input-address-back')" />
-                    <select class="location-selector w-full outline-none" wire.model="data.{{ $index }}.back_location_id"
-                        name="address[{{ $index }}][back_location_id]"></select>
-                    <x-form.error :messages="$errors['address.'.$index.'.back_location_id'][0] ?? ''" />
+                    <select data-key="back_location_id" class="location-selector w-full outline-none"
+                        wire:model="data.{{ $index }}.back_location_id"
+                        name="address[{{ $index }}][back_location_id]">
+                        @isset($old[$index]['back_location_id'])
+                            <option value="{{ $old[$index]['back_location_id'] }}" selected>
+                                {{ $locations[$old[$index]['back_location_id']] }}</option>
+                        @endisset
+                    </select>
+                    <x-form.error :messages="$errors['address.' . $index . '.back_location_id'][0] ?? ''" />
                 </section>
                 <section class="space-y-2"> {{-- BACK_DATE --}}
                     <x-form.label for="data.{{ $index }}.back_date" :value="__('budgets.input-address-back-datetime')" />
                     <div class="flex space-x-2">
-                        <x-form.input id="data.{{ $index }}.back_date"
-                            name="address[{{ $index }}][back_date]" type="datetime-local" class="block w-full"
-                            :value="$old[$index]['back_date'] ?? ''" />
+                        <x-form.input id="data.{{ $index }}.back_date" :value="$old[$index]['back_date'] ?? ''"
+                            wire:model="data[{{ $index }}][back_date]"
+                            name="address[{{ $index }}][back_date]" type="datetime-local"
+                            class="block w-full" />
                         @if ($index !== 0)
                             <div>
                                 <x-button type="button" wire:click.prevent="removeAddress({{ $index }})"
@@ -35,7 +49,7 @@
                             </div>
                         @endif
                     </div>
-                    <x-form.error :messages="$errors['address.'.$index.'.back_date'][0] ?? ''" />
+                    <x-form.error :messages="$errors['address.' . $index . '.back_date'][0] ?? ''" />
                 </section>
             </section>
         @endforeach
