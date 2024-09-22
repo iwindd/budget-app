5fbbf878d4a6edd34cab67181b87292a283ff794
iwindd <freewinendlife@gmail.com>
2024-09-22 14:29:23 +0700

    feat: livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'K UI') }}</title>
+    <title>{{ $title ?? 'Untitled' }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -60,7 +60,7 @@
                 <!-- Page Heading -->
                 <header>
                     <div class="p-4 sm:p-6">
-                        {{ $header }}
+                        {{ $header ?? "" }}
                     </div>
                 </header>
 
