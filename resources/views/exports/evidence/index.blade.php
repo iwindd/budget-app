e2021e27a4f265945c2d8bf49152a556e72dd374
iwindd <freewinendlife@gmail.com>
2024-09-29 01:40:41 +0700

    feat<export-evidence>: footer

--- resources/views/exports/evidence/index.blade.php
+++ resources/views/exports/evidence/index.blade.php
@@ -49,6 +49,9 @@
 
 <body>
     <h1 class="title">{{ __('exports.evidence-title') }}</h1>
+    @php
+        $expenses_total = 0;
+    @endphp
 
     <section>
         <table class="w-5 push-center">
@@ -82,9 +85,9 @@
                     <th class="w-1" rowspan="2">{{ __('exports.evidence-table-position') }}</th>
                     <th colspan="{{$listExpenses->count()}}">{{ __('exports.evidence-table-expenses') }}</th>
                     <th style="width: 5%;" rowspan="2">{{ __('exports.evidence-table-total') }}</th>
-                    <th class="w-2" rowspan="2">{{ __('exports.evidence-table-signature') }}</th>
+                    <th class="w-1" rowspan="2">{{ __('exports.evidence-table-signature') }}</th>
                     <th class="w-1" rowspan="2">{{ __('exports.evidence-table-date') }}</th>
-                    <th rowspan="2">{{ __('exports.evidence-table-note') }}</th>
+                    <th class="w-1" rowspan="2">{{ __('exports.evidence-table-note') }}</th>
                 </tr>
                 <tr>
                     @foreach ($listExpenses as $item)
@@ -116,9 +119,6 @@
                 @endforeach
                 <tr>
                     <th colspan="3">{{__('exports.evidence-table-total-all')}}</th>
-                    @php
-                        $expenses_total = 0;
-                    @endphp
                     @foreach ($listExpenses as $expense)
                         @php
                             $expense_total = 0;
@@ -146,6 +146,61 @@
                 </tr>
             </tbody>
         </table>
+    </section>
+
+    <section style="margin-top: 1em;">
+        <section style="float:left;" class="w-6">
+            <table class="push-left w-8">
+                <tr>
+                    <td class="fit">{{ __('exports.evidence-total-money') }}</td>
+                    <td class="under"><span>{{$format->bahtText($expenses_total)}}</span></td>
+                </tr>
+            </table>
+            <table class="text-sm-all" style="overflow: hidden;">
+                <tr>
+                    <td class="fit underline" style="vertical-align: top;">{{ __('exports.evidence-explanation') }}</td>
+                    <td class="fit" style="vertical-align: top; padding: 0 0.2em;">1.</td>
+                    <td class="grow" style="text-align: left; vertical-align: top;">
+                        {{ __('exports.evidence-explanation-1') }}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="fit" style="vertical-align: top; padding: 0 0.2em;">2.</td>
+                    <td class="grow" style="text-align: left; vertical-align: top;">
+                        {{ __('exports.evidence-explanation-2') }}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="fit" style="vertical-align: top; padding: 0 0.2em;">3.</td>
+                    <td class="grow" style="text-align: left; vertical-align: top;">
+                        {{ __('exports.evidence-explanation-3') }}</td>
+                </tr>
+            </table>
+        </section>
+        <section style="float:right;" class="w-4">
+            <table class="push-center w-8">
+                <tr>
+                    <td class="fit text-right">{{ __('exports.document-named') }}</td>
+                    <td class="under"><span></span></td>
+                    <td class="fit">{{ __('exports.document-payer') }}</td>
+                </tr>
+                <tr>
+                    <td class="fit text-right">(</td>
+                    <td class="under"><span></span></td>
+                    <td class="fit">)</td>
+                </tr>
+                <tr>
+                    <td class="fit text-right">{{ __('exports.document-position') }}</td>
+                    <td class="under"><span></span></td>
+                    <td class="fit"></td>
+                </tr>
+                <tr>
+                    <td class="fit text-right">{{ __('exports.document-date') }}</td>
+                    <td class="under"><span></span></td>
+                    <td class="fit"></td>
+                </tr>
+            </table>
+        </section>
     </section>
 </body>
 
