87a7705059477eb7702d432a844e38395eeba371
iwindd <freewinendlife@gmail.com>
2024-09-28 19:18:04 +0700

    fix<budget>: remove auto get

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -80,7 +80,7 @@ class Budget extends Model
     public static function getExpenses(Budget $budget) {
         return Expense::whereHas('budgetItemExpenses.budgetItem', function ($query) use ($budget) {
             $query->where('budget_id', $budget->id); // Correctly filter by budget_id in BudgetItem
-        })->get();
+        });
     }
 
     /**
