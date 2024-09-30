97438e6f3453ff7838dab9fed42cc606177846bb
iwindd <freewinendlife@gmail.com>
2024-09-30 11:53:02 +0700

    feat<dropdown-component>: teleport method

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,7 +1,8 @@
 @props([
     'align' => 'right',
     'width' => '48',
-    'contentClasses' => 'py-1 bg-white dark:bg-dark-eval-2'
+    'contentClasses' => 'py-1 bg-white dark:bg-dark-eval-2',
+    'teleport' => false,
 ])
 
 @php
@@ -25,30 +26,34 @@
     }
 @endphp
 
-<div
-    class="relative"
-    x-data="{ open: false }"
-    x-on:click.away="open = false"
-    x-on:close.stop="open = false"
->
+<div class="relative" x-data="{ open: false, triggerPosition: {} }" x-on:click.away="open = false" x-on:close.stop="open = false"
+    x-init="$watch('open', value => {
+        if (value) {
+            const triggerEl = $el.querySelector('[x-on\\:click]');
+            triggerPosition = triggerEl.getBoundingClientRect();
+        }
+    });">
     <div x-on:click="open = ! open">
         {{ $trigger }}
     </div>
 
-    <div
-        x-show="open"
-        x-transition:enter="transition ease-out duration-200"
-        x-transition:enter-start="transform opacity-0 scale-95"
-        x-transition:enter-end="transform opacity-100 scale-100"
-        x-transition:leave="transition ease-in duration-75"
-        x-transition:leave-start="transform opacity-100 scale-100"
+    @if ($teleport)
+        @teleport('#teleport-wrapper')
+    @endif
+    <div x-show="open" x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95"
         class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
         style="display: none;"
-        x-on:click="open = false"
-    >
+        @if ($teleport) x-bind:style="`top: ${triggerPosition.bottom + window.scrollY}px; left: ${triggerPosition.left + window.scrollX}px; `" @endif
+        x-on:click="open = false">
         <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
             {{ $content }}
         </div>
     </div>
+    @if ($teleport)
+        @endteleport('#teleport-wrapper')
+    @endif
+
 </div>
