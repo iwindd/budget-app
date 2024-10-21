a878425168f3e6e870caf5638c25097ae1229448
iwindd <freewinendlife@gmail.com>
2024-10-21 10:52:44 +0700

    fix<budget/expense>: user cannot create new expense

--- app/Livewire/Forms/BudgetItemExpenseForm.php
+++ app/Livewire/Forms/BudgetItemExpenseForm.php
@@ -4,6 +4,8 @@ namespace App\Livewire\Forms;
 
 use App\Models\BudgetItem;
 use App\Models\BudgetItemExpense;
+use App\Models\Expense;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Form;
 
 class BudgetItemExpenseForm extends Form
@@ -13,17 +15,34 @@ class BudgetItemExpenseForm extends Form
     public $days;
     public $total;
 
-    public function clear() {
+    public function clear()
+    {
         $this->reset();
         $this->getComponent()->dispatch("onExpenseSelectorClear");
     }
 
-    public function delete(BudgetItemExpense $budgetItemExpense) {
+    public function delete(BudgetItemExpense $budgetItemExpense)
+    {
         $budgetItemExpense->delete();
+    }
+
+    private function create()
+    {
+        $validated = $this->validate([
+            'expense_id' => ['required', 'string', 'max:255'],
+        ]);
+
+        return Expense::create([
+            'user_id' => Auth::user()->id,
+            'label' => $validated['expense_id'],
+            'merge' => true,
+            'default' => false
+        ]);
     }
 
     public function save(BudgetItem $budgetItem)
     {
+        if (is_string($this->expense_id)) $this->expense_id = $this->create()->id;
         $validated = $this->validate([
             'expense_id' => ['required', 'integer', 'exists:expenses,id'],
             'days' => ['nullable', 'integer', 'min:0'],
