3fd89ee873783189bfcd172132e0952e0f65193d
iwindd <freewinendlife@gmail.com>
2024-10-22 22:45:30 +0700

    (feat, fix)<text-field>: fix errors, wrapper props, feat label link

--- resources/views/components/textfield.blade.php
+++ resources/views/components/textfield.blade.php
@@ -1,11 +1,12 @@
 @props([
     'disabled' => false,
     'placeholder' => '',
+    'id' => '',
     'startIcon' => null,
     'endIcon' => null,
     'label' => null,
     'helper' => null,
-    'errors' => null,
+    'error' => null,
     'bag' => null,
     'wrapperProps' => []
 ])
@@ -13,7 +14,7 @@
 @php
     $wrapperClasses = "
         flex border rounded-md justify-center items-center dark:bg-dark-eval-1 dark:disabled:bg-dark-eval-0 overflow-hidden
-        " . ($errors ? 'border-danger dark:border-danger' : 'border-gray-400 focus-within:border-primary dark:border-gray-600') . "
+        " . ($error ? 'border-danger dark:border-danger' : 'border-gray-400 focus-within:border-primary dark:border-gray-600 dark:focus-within:border-primary') . "
         disabled:bg-gray-200 w-full
         gap-2 px-2
     ";
@@ -22,9 +23,10 @@
 <section>
     @if ($label)
         <label
+            for="{{$id}}"
             class="
                 block font-medium text-sm
-                {{$errors ? 'text-danger' : 'text-gray-700 dark:text-gray-300'}}
+                {{$error ? 'text-danger' : 'text-gray-700 dark:text-gray-300'}}
             "
         >
             {{ $label }}
@@ -32,13 +34,13 @@
     @endif
     <section
         {!!
-            $attributes->merge($wrapperProps)->merge([
+            $attributes->only($wrapperProps)->merge([
                 'class' => $wrapperClasses
             ])
         !!}
     >
         @if ($startIcon)
-            <div class="w-6 h-6 text-gray-700 flex justify-center items-center">
+            <div class="w-6 h-6 text-gray-400 dark:text-gray-600 flex justify-center items-center">
                 {{ $startIcon }}
             </div>
         @endif
@@ -47,20 +49,21 @@
             {!!$attributes->merge([
                 'placeholder' => $placeholder,
                 'disabled' => $disabled,
+                'id' => $id,
                 'class' => 'py-2 px-0 dark:text-gray-300 border-0 ring-0 outline-0 focus:border-0 focus:outline-0 focus:ring-0 flex-grow',
             ])!!}
         />
 
         @if ($endIcon)
-            <div class="w-6 h-6 text-gray-700 flex justify-center items-center">
+            <div class="w-6 h-6 text-gray-500 flex justify-center items-center">
                 {{ $endIcon }}
             </div>
         @endif
     </section>
 
-    @if ($errors)
+    @if ($error)
         <ul class="block font-medium text-xs text-danger">
-            @foreach ((array) $errors as $message)
+            @foreach ((array) $error as $message)
                 <li>{{ $message }}</li>
             @endforeach
         </ul>
@@ -69,7 +72,7 @@
             <label
                 class="
                     block font-medium text-xs
-                    {{$errors ? 'text-danger' : 'text-gray-700 dark:text-gray-300'}}
+                    {{$error ? 'text-danger' : 'text-gray-700 dark:text-gray-300'}}
                 "
             >
                 {{ $helper }}
