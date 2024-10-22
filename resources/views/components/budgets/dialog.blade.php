00c8e1130992fab4cda3077fef5a9b6a5257fb11
iwindd <freewinendlife@gmail.com>
2024-10-23 00:29:31 +0700

    refactor<budget>: dialog

--- resources/views/components/budgets/dialog.blade.php
+++ resources/views/components/budgets/dialog.blade.php
@@ -1,15 +1,17 @@
 <x-modal name="find-budget-form" focusable maxWidth="md" :show="$errors->findBudget->isNotEmpty()">
     <form class="p-6" method="post" action="{{ route('budgets.find') }}">
         @csrf
-        <h2 class="text-lg font-medium" x-text="text"></h2>
+        <h2 class="text-lg font-medium">{{__('budgets.input-serial')}}</h2>
 
         <div class="mt-6 space-y-6">
-            <x-form.label for="serial" value="Password" class="sr-only" />
-
-            <x-form.input id="serial" name="serial" type="text" class="block w-full"
-                placeholder="{{ __('budgets.input-serial') }}" required />
-
-            <x-form.error :messages="$errors->findBudget->get('serial')" />
+            <x-textfield
+                :error="$errors->findBudget->get('serial')"
+                type="text"
+                id="serial"
+                name="serial"
+                required
+                autofocus
+            />
         </div>
 
         <div class="mt-6 flex justify-end">
