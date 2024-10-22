0e5dff96e53222a4fe1ca81696bc0080ed7767b0
iwindd <freewinendlife@gmail.com>
2024-10-22 19:58:54 +0700

    refactor<budget-form>: order

--- resources/views/livewire/budgets/index.blade.php
+++ resources/views/livewire/budgets/index.blade.php
@@ -1,20 +1,21 @@
 @inject('format', 'App\Services\FormatHelperService')
 
 <section class="space-y-2">
-    <form wire:submit="save" class="space-y-2">
+    <form wire:submit="save" class="space-y-2" id="budget-form">
         @include('livewire.budgets.partials.budget')
         @include('livewire.budgets.partials.budget-item')
-
-        <div class="p-2 sm:p-2 bg-white shadow sm:rounded-lg dark:bg-gray-800">
-            <div class="max-w-full">
-                <section class="max-w-full col-span-2 flex justify-end">
-                    <x-button type="submit" variant="success">บันทึกใบเบิก</x-button>
-                </section>
-            </div>
-        </div>
     </form>
 
     @include('livewire.budgets.partials.companion')
+
+    <div class="p-2 sm:p-2 bg-white shadow sm:rounded-lg dark:bg-gray-800 mt-2">
+        <div class="max-w-full">
+            <section class="max-w-full col-span-2 flex justify-end">
+                <x-button type="submit" form="budget-form" variant="success">บันทึกใบเบิก</x-button>
+            </section>
+        </div>
+    </div>
+
     @include('livewire.budgets.partials.address')
     @include('livewire.budgets.partials.expense')
     @include('livewire.budgets.extras.travel')
