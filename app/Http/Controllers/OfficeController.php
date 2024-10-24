cd9c8cef2bb7bff8afc01e66800f558c17624f30
iwindd <freewinendlife@gmail.com>
2024-10-24 21:59:46 +0700

    refactor<OfficeController>: remove useless method

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -16,46 +16,4 @@ class OfficeController extends Controller
     {
         return view('admin.offices.index');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreOfficeRequest $request)
-    {
-        if ($request->validated()['default']) Office::deactivated();
-
-        $office = $this->auth()->offices()->create($request->validated());
-
-        return Redirect::back()->with('alert', [
-            'text' => trans("offices.controller-store", ["label" => $office->label]),
-            'variant' => "success"
-        ]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateOfficeRequest $request, Office $office)
-    {
-        if ($request->validated()['default']) Office::deactivated();
-        $office->update($request->validated());
-
-        return Redirect::back()->with('alert', [
-            'text' => trans("offices.controller-update", ["label" => $office->label]),
-            'variant' => "success"
-        ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Office $office)
-    {
-        $office->delete();
-
-        return Redirect::back()->with('alert', [
-            'text' => trans("offices.controller-destroy", ["label" => $office->label]),
-            'variant' => "success"
-        ]);
-    }
 }
