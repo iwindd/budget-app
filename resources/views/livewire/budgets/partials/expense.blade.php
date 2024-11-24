fc02df88465274f54535b3b0f00ef74fdf975640
iwindd <freewinendlife@gmail.com>
2024-11-24 13:34:35 +0700

    feat: expenses hint

--- resources/views/livewire/budgets/partials/expense.blade.php
+++ resources/views/livewire/budgets/partials/expense.blade.php
@@ -1,12 +1,11 @@
 <div class="max-w-full">
     @if ($hasPermissionToManage)
         <form wire:submit="onAddExpense" class="grid grid-cols-5 gap-1 mb-2 border-b pb-2">
-            <x-textfield value="เพิ่มค่าใช้จ่ายอื่นๆ" disabled class="lg:pl-3 pl-0" :root="['class'=>'col-span-2 lg:col-span-1']" />
             <x-selectize
                 lang="expenses.selectize"
                 :fetch="route('expenses.selectize')"
                 wire:model="budgetExpenseForm.expense"
-                :root="['class' => 'space-y-1 col-span-3 lg:col-span-2']"
+                :root="['class' => 'space-y-1 col-span-4 lg:col-span-3']"
                 create
                 :parseInt="false"
                 :parseCreate="true"
@@ -18,7 +17,7 @@
                 type="number"
                 :root="['class' => 'space-y-1 col-span-3 lg:col-span-1']"
             />
-            <div class="col-span-2 lg:col-span-1">
+            <div class="col-span-2 lg:col-span-1 pt-6">
                 <x-button type="submit" name="submit" class="w-full justify-center truncate">{{ __('budgets.add-expense-btn') }}</x-button>
             </div>
         </form>
@@ -26,7 +25,7 @@
 
     @foreach ($expenses as $index => $expense)
         <div
-            class="grid grid-cols-4 gap-1"
+            class="grid grid-cols-4 gap-1 p-0"
             wire:key="{{$expense['id']}}"
             x-data="{
                 days: @entangle("expenses.$index.days"),
@@ -42,7 +41,7 @@
             }"
         >
             <div class="{{!$expense['split'] ? "col-span-2" : ""}}">
-                <x-textfield :value="$expense['label']" disabled class="lg:pl-3 pl-0" />
+                <x-textfield lang="expenses.input-label" :value="$expense['label']" disabled class="lg:pl-3 pl-0" />
             </div>
             @if ($expense['split'])
                 <x-textfield
@@ -65,13 +64,14 @@
 
                     <div class="flex gap-1 w-full h-full">
                         <x-textfield
+                            lang="expenses.input-sum"
                             disabled
                             x-bind:value="sum"
                             :root="['class'=>'flex-grow']"
                             class="text-end"
                         />
                         @if ($expense['merge'] && $hasPermissionToManage)
-                            <div>
+                            <div class="pt-5">
                                 <x-button wire:click="onRemoveExpense({{$expense['id']}})" type="button" icon-only variant="danger" size="sm">
                                     <x-heroicon-o-trash class="w-7 h-full" />
                                 </x-button>
