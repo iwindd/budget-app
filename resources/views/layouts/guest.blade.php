f676a68985a0b68117538e1760ab791612c5bf6f
iwindd <freewinendlife@gmail.com>
2024-12-09 12:13:43 +0700

    feat: remove dark mode

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -28,35 +28,12 @@
     <div
         x-data="mainState"
         class="font-sarabun antialiased"
-        :class="{dark: isDarkMode}"
         x-cloak
     >
         <div class="flex flex-col min-h-screen text-gray-900 bg-gray-100 dark:bg-dark-eval-0 dark:text-gray-200">
             {{ $slot }}
 
             <x-footer />
-        </div>
-
-        <div class="fixed top-10 right-10">
-            <x-button
-                type="button"
-                icon-only
-                variant="secondary"
-                sr-text="Toggle dark mode"
-                x-on:click="toggleTheme"
-            >
-                <x-heroicon-o-moon
-                    x-show="!isDarkMode"
-                    aria-hidden="true"
-                    class="w-6 h-6"
-                />
-
-                <x-heroicon-o-sun
-                    x-show="isDarkMode"
-                    aria-hidden="true"
-                    class="w-6 h-6"
-                />
-            </x-button>
         </div>
     </div>
     @livewireScripts
